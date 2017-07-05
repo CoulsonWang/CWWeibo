9d95349fe343ecde2d1f442c3d1904ed32198699
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-05 13:33:00 +0800

    使用storyboard refrence搭建项目框架

---
+++ CWWeibo/CWWeibo/Classes/Main/WBMainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WBMainViewController.swift
+//  CWWeibo
+//
+//  Created by Coulson_Wang on 2017/7/5.
+//  Copyright © 2017年 Coulson_Wang. All rights reserved.
+//
+
+import UIKit
+
+class WBMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+//        guard let jsonPath = Bundle.main.path(forResource: "MainVCSettings", ofType: "json") else {
+//            return
+//        }
+//        guard let jsonData = NSData(contentsOfFile: jsonPath) else {
+//            return
+//        }
+//        
+//        guard let anyObject = try? JSONSerialization.jsonObject(with: jsonData as Data, options: .mutableContainers) else {
+//            return
+//        }
+//        
+//        guard let dictArray = anyObject as? [[String : Any]] else {
+//            return
+//        }
+//        
+//        for dict in dictArray {
+//            guard let vcName = dict["vcName"] as? String else {
+//                continue
+//            }
+//            guard let title = dict["title"] as? String else {
+//                continue
+//            }
+//            guard let imageName = dict["imageName"] as? String else {
+//                continue
+//            }
+//        }
+    
+
+}
