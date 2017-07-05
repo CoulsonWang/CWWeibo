ec6778243be816f01121d796cfad9c747beeec4a
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-05 17:02:06 +0800

    处理游客界面显示 1.布局视图 2.设置不同界面的视图属性 3.设置home界面动画

--- CWWeibo/CWWeibo/Classes/Main/Visitor/WBVisitorView.swift
+++ CWWeibo/CWWeibo/Classes/Main/Visitor/WBVisitorView.swift
@@ -13,4 +13,28 @@ class WBVisitorView: UIView {
     class func visitorView() -> WBVisitorView {
         return Bundle.main.loadNibNamed("WBVisitorView", owner: nil, options: nil)?.first as! WBVisitorView
     }
+    
+    // MARK:- 控件的属性
+    @IBOutlet weak var rotationView: UIImageView!
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    // MARK:- 自定义函数
+    func setupVisitorViewInfo(iconImage : UIImage, title : String) {
+        iconView.image = iconImage
+        tipLabel.text = title
+        rotationView.isHidden = true
+    }
+    
+    func addRotationAnim() {
+        let rotationAnim = CABasicAnimation(keyPath: "transform.rotation.z")
+        
+        rotationAnim.fromValue = 0
+        rotationAnim.toValue = 2 * Double.pi
+        rotationAnim.repeatCount = MAXFLOAT
+        rotationAnim.duration = 5
+        rotationAnim.isRemovedOnCompletion = false
+        
+        rotationView.layer .add(rotationAnim, forKey: nil)
+    }
 }
