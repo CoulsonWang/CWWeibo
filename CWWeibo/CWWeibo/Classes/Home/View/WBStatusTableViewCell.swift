fe30f842b64970c3e00ddd0cc7df0216551d78cf
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-07 13:59:23 +0800

    布局cell界面

--- CWWeibo/CWWeibo/Classes/Home/View/WBStatusTableViewCell.swift
+++ CWWeibo/CWWeibo/Classes/Home/View/WBStatusTableViewCell.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class WBStatusTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var textLabelConstraintWidth: NSLayoutConstraint!
+    @IBOutlet weak var textLabelConstraintLeftMargin: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        textLabelConstraintWidth.constant = UIScreen.main.bounds.width - 2 * textLabelConstraintLeftMargin.constant
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
